508e0c4969dcf4573d62232a464fa91d67c9509e
mxy <imxyi@hotmail.com>
2023-02-01 16:59:32 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  presets: ['@babel/preset-typescript', '@babel/preset-env', 'solid'],
+  plugins: ['@babel/plugin-transform-runtime'],
+};
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Component } from 'solid-js';
+
+const App: Component = () => {
+  return <div>hello solid-js webpack</div>;
+};
+
+
+export default App;
